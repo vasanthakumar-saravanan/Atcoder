221e8ee1071b0949440b48448b047cece950307a
Vasanthakumar Saravanan <vasanthakumar.saravanan.01@gmail.com>
2026-07-28 09:47:03 +0530

    Add ABC 463 Task A

--- ABC463/ABC_463_Task_A.java
+++ ABC463/ABC_463_Task_A.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ABC_463_Task_A{
     public static void  main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);            //https://atcoder.jp/contests/abc463/tasks/abc463_a
         int n =sc.nextInt();
         int m = sc.nextInt();
         if(n%16 ==0 && m%9 ==0){
